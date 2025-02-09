532822ebcdadd49a466e212943acba47459d5c2b
Luiz Faraco <wduty@hotmail.com>
2025-02-08 22:24:24 -0300

    Correção testes unitarios

--- Domain/DTO/DTONotificacao.cs
+++ Domain/DTO/DTONotificacao.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.DTO
+﻿namespace Domain.DTO
 {
     public class DTONotificacao
     {
         public string? EmailMedico { get; set; }
-
         public string? EmailPaciente { get; set; }
-
         public string? NomePaciente { get; set; }
-
         public string? NomeMedico { get; set; }
-
         public string? HorarioConsulta { get; set; }
-
         public bool? Confirmacao { get; set; }
+
+        public override string ToString() => $"EmailMedico: {EmailMedico}; EmailPaciente: {EmailPaciente}; NomePaciente: {NomePaciente}; NomeMedico: {NomeMedico}; HorarioConsulta: {HorarioConsulta}; Confirmacao: {Confirmacao}; ";
     }
 }
--- Service/Service/ServiceNotificacao.cs
+++ Service/Service/ServiceNotificacao.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.DTO;
 using Domain.Interfaces.Service;
 using MassTransit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service.Service
 {
