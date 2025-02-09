9a725cb53c19bff0679734194b94f341c6503404
Luiz Faraco <wduty@hotmail.com>
2025-02-08 22:57:15 -0300

    Ajuste horario medico

--- Tests/TestValidacoes.cs
+++ Tests/TestValidacoes.cs
@@ -55,19 +55,19 @@ public class TestValidacoes
         periodo = new(-1, 18);
         Assert.ThrowsAny<Exception>(() => periodo.Validar());
 
-        periodo = new(0, 24);
+        periodo = new(0, 23);
         periodo.Validar();
 
-        periodo = new(12.1, 12);
+        periodo = new(12, 12);
         Assert.ThrowsAny<Exception>(() => periodo.Validar());
 
-        periodo = new(0, 24.0001);
+        periodo = new(0, 24);
         Assert.ThrowsAny<Exception>(() => periodo.Validar());
 
         var conflitos = Periodo.ChecaConflitos(new Periodo(8, 18), new Periodo(8, 18));
-        Assert.Equal(conflitos?.periodo1.HoraInicial.Hours, 8);
+        Assert.Equal(conflitos?.periodo1.HoraInicial, 8);
 
         conflitos = Periodo.ChecaConflitos(new Periodo(0, 2), new Periodo(4, 6), new Periodo(1, 3));
-        Assert.Equal(conflitos?.periodo2.HoraInicial.Hours, 1);
+        Assert.Equal(conflitos?.periodo2.HoraInicial, 1);
     }
 }
